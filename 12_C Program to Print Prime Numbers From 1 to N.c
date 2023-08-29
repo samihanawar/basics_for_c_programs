d70fb289c5a231e8862590715570bb91cd8f99cc
samihanawar <138853046+samihanawar@users.noreply.github.com>
2023-08-29 19:23:52 +0600

    Update 12_C Program to Print Prime Numbers From 1 to N.c

--- 12_C Program to Print Prime Numbers From 1 to N.c
+++ 12_C Program to Print Prime Numbers From 1 to N.c
@@ -20,30 +20,5 @@ int main()
   return 0;
 }
 
-/*#include<stdio.h>
-main() {
-    int i,flag,number;
-    flag=0;
-    printf("Enter the number:");
-    scanf("%d",&number);
-    for(i=1;i<=number;i++){
-        for(i=2; i<number; i++)
-        {    if (number%i==0)
-            {
-            flag=1;
-            };
-        }
-        if (flag==0)
-        {
-        printf("\nThe number %d is prime", number);
-        }
-        else
-        {
-        printf("\nThe number %d is not prime\n", number);
-        }
-    }
 
-
-}
-*/
 
